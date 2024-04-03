341b41277572ce5fe39dd3f6cabb57df2f09c1b9
RubenDarioJ <rubendjusto02@gmail.com>
2024-04-03 19:29:32 -0400

    first commit

---
+++ src/components/Images.vue
@@ -0,0 +1,152 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const images = ref<Object>([
+  {
+    id: 1,
+    title: 'Anime girl',
+    url: 'youtube.com',
+    img: './src/assets/images/img.jpg',
+    isFav: true,
+    rating: 'Safe'
+  },
+  {
+    id: 2,
+    title: 'Anime girl',
+    url: 'youtube.com',
+    img: './src/assets/images/img2.png',
+    isFav: true,
+    rating: 'Safe'
+  },
+  {
+    id: 3,
+    title: 'Anime girl',
+    url: 'youtube.com',
+    img: './src/assets/images/img3.jpg',
+    isFav: false,
+    rating: 'Safe'
+  },
+  {
+    id: 4,
+    title: 'Anime girl',
+    url: 'youtube.com',
+    img: './src/assets/images/img4.jpg',
+    isFav: false,
+    rating: 'NSFW'
+  },
+  {
+    id: 5,
+    title: 'Anime girl',
+    url: 'youtube.com',
+    img: './src/assets/images/img5.jpg',
+    isFav: false,
+    rating: 'NSFW'
+  }
+])
+
+const imageCopy = ref([{ ...images }])
+console.log(imageCopy)
+
+const filter = ref({
+  filterById(userId) {
+    const filteredDataById = images.value.findIndex((user) => user.id === userId)
+    console.log(filteredDataById)
+    images.value.splice(filteredDataById, 1)
+  },
+  filterNSFW(rating) {
+    images.value = images.value.filter((element) => {
+      return element.rating === rating
+    })
+    toggleNSFWVisibility()
+  },
+  changeColor(image) {
+    image.isFav = !image.isFav
+  }
+})
+
+const showNSFW = ref<boolean>(true)
+
+const toggleNSFWVisibility = () => {
+  showNSFW.value = !showNSFW.value
+  // if (showNSFW.value) {
+  //   images.value = [...imageCopy.value]
+  // }
+}
+</script>
+
+<template>
+  <div>
+    <ul>
+      <button class="NSFWFilter" @click="filter.filterNSFW('Safe')">
+        {{ showNSFW ? 'Filtrar NSFW' : 'Mostrar NSFW' }}
+      </button>
+      <!-- <button class="displayNSFW">Display NSFW</button> -->
+      <li
+        v-for="image in images"
+        v-bind:class="{ fav: image.isFav }"
+        @click="filter.changeColor(image)"
+      >
+        <img :src="image.img" alt="" />
+        <p>{{ image.title }}</p>
+        <p>{{ image.rating }}</p>
+        <button @click="filter.filterById(image.id)">Borrar</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+li {
+  list-style-type: none;
+  margin: 20px auto;
+  padding: 10px 20px;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  cursor: pointer;
+}
+
+img {
+  background-color: #333;
+  object-fit: cover;
+  width: 150px;
+  aspect-ratio: 1/1;
+  border-radius: 50%;
+  margin: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  cursor: pointer;
+  transition: all 0.7s;
+}
+
+button {
+  cursor: pointer;
+  background-color: #ffff00;
+  border-radius: 12px;
+  color: #000;
+  cursor: pointer;
+  font-weight: bold;
+  padding: 10px 15px;
+  text-align: center;
+  transition: 200ms;
+  box-sizing: border-box;
+  border: 0;
+  font-size: 16px;
+  user-select: none;
+  touch-action: manipulation;
+}
+
+.NSFWFilter {
+  display: block;
+  margin: 0 auto;
+  margin-bottom: 10px;
+}
+
+.displayNSFW {
+  display: block;
+  margin: 0 auto;
+}
+
+li.fav {
+  background-color: grey;
+}
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+defineProps({
+  title: String
+})
+
+// const nameInputRef = ref<HTMLInputElement | null>(null);
+
+// const handleClick = () => {
+//   if (nameInputRef.value) {
+//     console.log(nameInputRef.value.value);
+//   }
+// };
+
+const ahorros = ref<number>(500)
+
+const sumar = (cantidad: number) => {
+  ahorros.value += cantidad
+  console.log(ahorros)
+}
+const restar = (cantidad: number) => {
+  ahorros.value -= cantidad
+  console.log(ahorros)
+}
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <h2 :class="ahorros >= 1000 ? 'positive' : ahorros === 0 ? 'negative' : ''">
+    Ahorros: {{ ahorros }}
+  </h2>
+  <button @click="restar(100)" :disabled="ahorros <= 0">Restar</button>
+  <button @click="sumar(100)" :disabled="ahorros >= 1000">Sumar</button>
+  <!-- <input type="text" ref="name" v-model="nameInputRef" />
+  <button @click="handleClick">Click me</button> -->
+</template>
+
+<style scoped>
+.negative {
+  color: red;
+}
+
+.positive {
+  color: green;
+}
+
+button {
+  background-color: #fff000;
+  border-radius: 12px;
+  color: #000;
+  cursor: pointer;
+  font-weight: bold;
+  padding: 10px 15px;
+  text-align: center;
+  transition: 200ms;
+  box-sizing: border-box;
+  border: 0;
+  font-size: 16px;
+  user-select: none;
+  touch-action: manipulation;
+  margin-right: 10px;
+}
+
+button:disabled,
+button[disabled]{
+  border: 1px solid #999999;
+  background-color: #cccccc;
+  color: #515151;
+}
+</style>
